ed89f3d6a96949cbe364960a816951acb9491a0b
Kaabocha <milky42744274@gmail.com>
2020-12-23 20:39:18 +0900

    単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,9 @@ class User < ApplicationRecord
   validates :nick_name, presence: true
   validates :email, presence: true
   validates :email, uniqueness: true
-  validates :family_name, presence: true,format: {with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :first_name, presence: true,format: {with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :family_name_kana, presence: true,format: {with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/}
-  validates :first_name_kana, presence: true,format: {with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/}
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+  validates :family_name_kana, presence: true, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/ }
   validates :birthday, presence: true
 end
