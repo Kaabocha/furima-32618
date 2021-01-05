f21df8401b00587eff69a838a0e4e4e828fced3f
Kaabocha <milky42744274@gmail.com>
2021-01-05 12:42:28 +0900

    販売手数料と利益の計算

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,16 +1,16 @@
 window.addEventListener('load', function(){
 
 const priceInput = document.getElementById("item-price");
-priceInput.addEventListener("input", () => {
+  priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value
     console.log(inputValue);
-})
 
-const pricetaxInput = document.getElementById("add-tax-price");
-addTaxPrice.innerHTML = "入力した金額をもとに販売手数料を計算する処理"
-console.log(pricetaxInput);
+  const priceTax = document.getElementById("add-tax-price");
+  priceTax.innerHTML = Math.floor(priceInput.value * 0.1)
+  console.log(priceInput);
 
-
-const priceprofitInput = document.getElementById("profit");
-console.log(priceprofitInput);
+  const priceProfit = document.getElementById("profit");
+  priceProfit.innerHTML = Math.floor(priceInput.value * 0.9)
+  console.log(priceTax);
+  })
 })
