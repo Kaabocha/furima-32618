a1a0ccdb64427d0c9c6f19b794313ad67539f348
Kaabocha <milky42744274@gmail.com>
2021-01-07 18:44:43 +0900

    editにも条件式を追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-
+  before_action :set_item, only: [:edit, :update, :show]
   def index
     @items = Item.includes(:user).order("created_at DESC") 
   end
@@ -19,15 +19,15 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
+    if current_user.id != @item.user_id  
+       return redirect_to root_path
+    end
   end
 
   def update
-    @item = Item.find(params[:id]) 
     if current_user.id == @item.user_id 
       @item.update(item_params)
       return redirect_to item_path if @item.valid?
@@ -42,10 +42,15 @@ class ItemsController < ApplicationController
 
   private
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   def item_params
     params.require(:item).permit(
       :item_name, :item_description, :category_id, :item_condition_id,
       :shipping_charge_id, :delivery_area_id, :days_to_ship_id, :price, :image
     ).merge(user_id: current_user.id)
   end
+
 end
