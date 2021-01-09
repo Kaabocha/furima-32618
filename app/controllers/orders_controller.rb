78645b9fb129e863ce49899ff5b9c83e51928457
Kaabocha <milky42744274@gmail.com>
2021-01-09 17:44:40 +0900

    コントローラーの変数修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,26 +2,26 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
-    @orderform = OrderForm.new
+    @orderstreet = OrderStreet.new
   end
 
   def create
-    binding.pry
-    @orderform = OrderForm.new(order_params)
-    if @orderform.valid?
-      @orderform.save
+    @orderstreet = OrderStreet.new(order_params)
+    # binding.pry
+    if @orderstreet.valid?
+      @orderstreet.save
       return redirect_to root_path
     else
+      @item = Item.find(params[:item_id])
       render 'index'
     end
   end
 
   private
 
-  def orderform_params
-    params.require(:order).permit(:user_id, :item_id, :postal_code, 
-      :delivery_area_id, :municipality, :address, :phone_number, :building, :order_id)
-      .merge(user_id: current_user.id, item_id: params[:item_id]) #ここのcurrent_user.idが使えるのはdeviseのGemを導入しているからです。
+  def order_params
+    params.require(:order_street).permit(:postal_code, :delivery_area_id, :municipality, :address, :phone_number, :building,)
+      .merge(user_id: current_user.id, item_id: params[:item_id ]) #ここのcurrent_user.idが使えるのはdeviseのGemを導入しているからです。
   end
 end
 
