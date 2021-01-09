b6c3a1fa30f8bc7514404cdcf81a1688e57ce31b
Kaabocha <milky42744274@gmail.com>
2021-01-09 17:45:40 +0900

    配送先のidを利用

--- app/models/street.rb
+++ app/models/street.rb
@@ -2,11 +2,11 @@ class Street < ApplicationRecord
 
   belongs_to :order
 
-  with_options presence: true do
-    validates :postal_code
-    validates :prefecture_id
-    validates :municipality
-    validates :address
-    validates :phone_number
-
+  # with_options presence: true do
+  #   validates :postal_code
+  #   validates :delivery_area_id
+  #   validates :municipality
+  #   validates :address
+  #   validates :phone_number
+  # end
 end
