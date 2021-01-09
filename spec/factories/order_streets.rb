2729df0bd56a11323ffc4c3ffbd4a1024b728c1c
Kaabocha <milky42744274@gmail.com>
2021-01-09 17:46:46 +0900

    order_streetのファクトリーボットを作成

---
+++ spec/factories/order_streets.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :order_street do
+    postal_code                      { "123-4567" }
+    delivery_area_id                 { 1 }
+    municipality                     { "横浜市美浜区" }
+    address                          { "柳ビル101" }
+    phone_number                     { "09012345678" }
+  end
+end
