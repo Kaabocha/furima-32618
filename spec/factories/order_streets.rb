3e58e98b95de630bf3601f37b867cc787c21e99c
Kaabocha <milky42744274@gmail.com>
2021-01-11 18:12:11 +0900

    ファクトリーボットにaddressとbuildingの値を追加&修正

--- spec/factories/order_streets.rb
+++ spec/factories/order_streets.rb
@@ -3,7 +3,8 @@ FactoryBot.define do
     postal_code                      { "123-4567" }
     delivery_area_id                 { 2 }
     municipality                     { "横浜市美浜区" }
-    address                          { "柳ビル101" }
+    address                          { "青山1-1-1" }
+    building                         { "" }
     phone_number                     { "09012345678" }
     token                            { "testtest" }
     user_id                          { 1 }
