4894baf038ff2868194cbbeda2c58243945cf9fb
Kaabocha <milky42744274@gmail.com>
2021-01-11 19:10:31 +0900

    クレカに関する動きの内容をメソッド化

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,12 +12,7 @@ class OrdersController < ApplicationController
   def create
     @orderstreet = OrderStreet.new(order_params)
     if @orderstreet.valid?
-      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]      # 自身のPAY.JPテスト秘密鍵を記述しましょう
-      Payjp::Charge.create(
-        amount: @item.price,                       # 商品の値段
-        card: order_params[:token],                # カードトークン
-        currency: 'jpy'                            # 通貨の種類（日本円）
-      )
+      payjp_info
       @orderstreet.save
       return redirect_to root_path
     else
@@ -35,6 +30,15 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order_street).permit(:postal_code, :delivery_area_id, :municipality, :address, :phone_number, :building,)
       .merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id ]) #ここのcurrent_user.idが使えるのはdeviseのGemを導入しているからです。
+  end
+
+  def payjp_info
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]      # 自身のPAY.JPテスト秘密鍵を記述する
+    Payjp::Charge.create(
+      amount: @item.price,                       # 商品の値段
+      card: order_params[:token],                # カードトークン
+      currency: 'jpy'                            # 通貨の種類（日本円）
+    )
   end
 
 end
