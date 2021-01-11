e660ad3e18a3175fd9910545b26f7814d204bd9f
Kaabocha <milky42744274@gmail.com>
2021-01-11 18:07:28 +0900

    間違えて記述していたバリデーションを削除

--- app/models/street.rb
+++ app/models/street.rb
@@ -2,11 +2,5 @@ class Street < ApplicationRecord
 
   belongs_to :order
 
-  # with_options presence: true do
-  #   validates :postal_code
-  #   validates :delivery_area_id
-  #   validates :municipality
-  #   validates :address
-  #   validates :phone_number
-  # end
 end
+
