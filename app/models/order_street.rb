d7e31a4c204588769e73fc535e18473cbc3f588a
Kaabocha <milky42744274@gmail.com>
2021-01-11 18:07:05 +0900

    idの選択が0以外の場合に変更

--- app/models/order_street.rb
+++ app/models/order_street.rb
@@ -5,7 +5,7 @@ class OrderStreet
 
   with_options presence: true do
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" } #郵便番号
-    validates :delivery_area_id,    numericality: { other_than: 0} #都道府県
+    validates :delivery_area_id,    numericality: { other_than: 1} #都道府県
     validates :municipality     #市区町村
     validates :address          #番地
     validates :phone_number, format: { with: /\A\d{10,11}\z/ }   #電話番号
