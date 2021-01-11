dd5529ee72cef4ac7780e009759de9ab17832a8f
Kaabocha <milky42744274@gmail.com>
2021-01-11 14:03:09 +0900

    トークンを.mergeに追加とcreateアクションにカードのトークンと環境変数・通貨の種類・商品の値段を追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -7,8 +7,14 @@ class OrdersController < ApplicationController
 
   def create
     @orderstreet = OrderStreet.new(order_params)
-    # binding.pry
+    @item = Item.find(params[:item_id])
     if @orderstreet.valid?
+      Payjp.api_key = ENV["PAYJP_SECRET_KEY"]      # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp::Charge.create(
+        amount: @item.price,                       # 商品の値段
+        card: order_params[:token],                         # カードトークン
+        currency: 'jpy'                                     # 通貨の種類（日本円）
+      )
       @orderstreet.save
       return redirect_to root_path
     else
@@ -21,7 +27,7 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_street).permit(:postal_code, :delivery_area_id, :municipality, :address, :phone_number, :building,)
-      .merge(user_id: current_user.id, item_id: params[:item_id ]) #ここのcurrent_user.idが使えるのはdeviseのGemを導入しているからです。
+      .merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id ]) #ここのcurrent_user.idが使えるのはdeviseのGemを導入しているからです。
   end
 end
 
