3ca9f9d8e5275491680b5cbb88b3c6d52eee23b5
Kaabocha <milky42744274@gmail.com>
2021-01-11 18:08:32 +0900

    ファクトリーボットのdelivery_area_idの仮値を2に修正

--- spec/factories/order_streets.rb
+++ spec/factories/order_streets.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :order_street do
     postal_code                      { "123-4567" }
-    delivery_area_id                 { 1 }
+    delivery_area_id                 { 2 }
     municipality                     { "横浜市美浜区" }
     address                          { "柳ビル101" }
     phone_number                     { "09012345678" }
